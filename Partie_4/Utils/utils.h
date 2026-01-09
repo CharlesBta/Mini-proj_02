a8b2d8136063d7e46a93459230c2266b491e8e38
Charles-Antoine <charto.bta@gmail.com>
2026-01-09 11:10:28 +0100

    Partie 4

---
+++ Partie_4/Utils/utils.h
@@ -0,0 +1,48 @@
+#define X_MIN -2.0
+#define Y_MIN 1.0
+#define X_MAX 1.0
+#define Y_MAX -1.0
+
+#define TARGET_X -0.99
+#define TARGET_Y 0.3
+
+#define LARGEUR 900
+#define HAUTEUR 600
+#define MAXVAL 255
+
+// structure de l'image
+typedef struct 
+{
+    char format[3]; // Format ex: "P3" + "\n" -> 3 chars
+    int largeur; // Largeur de l'image en ASCII + "\0" -> max 4 chars
+    int hauteur; // Hauteur de l'image en ASCII + "\0" -> max 4 chars
+    int maxval; // Valeur max des couleurs en ASCII + "\n" -> max 3 chars ex: "255"
+
+    double X_min;
+    double X_max;
+    double Y_min;
+    double Y_max;
+    float scale;
+    double pixwidth;
+    int* convrg;
+} Mandel_pic;
+
+typedef struct color {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+} color;
+
+void pixel_to_coord(int px, int py, double *x, double *y, double X_min, double Y_min, double X_max, double Y_max, int width, int height);
+
+Mandel_pic new_mandel(int width, int height, double Xmin, double Ymin, double scale, Mandel_pic* previous_mandel);
+
+int convergence(double x, double y);
+
+color palette(int c);
+
+int interpolate(Mandel_pic mandel, double x, double y);
+
+void save_mandel(const char* filename, Mandel_pic mandel);
+
+void free_mandel(Mandel_pic* mandel);
---
+++ Partie_4/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "./Utils/utils.h"
+#include <time.h>
+
+
+Mandel_pic generate_mandelbrot(double x, double y, double scale, char* filename, Mandel_pic* previous_mandel) {
+    Mandel_pic mandel = new_mandel(LARGEUR, HAUTEUR, x - (1.5 * scale), y + (1.0 * scale), scale, previous_mandel);
+    save_mandel(filename, mandel);
+    return mandel;
+}
+
+int main(void) {
+    double x = -0.743643887037151;
+    double y = 0.13182590420533;
+    double scale = 0.01;
+    char filename[256];
+    Mandel_pic* mandel = NULL;
+    Mandel_pic temp_mandel; 
+    for (int i = 1; i < 100; i++) {
+        sprintf(filename, "./output/mandelbrot_%d.ppm", i);
+        temp_mandel = generate_mandelbrot(x, y, scale, filename, mandel);
+        if (mandel == NULL) {
+            mandel = (Mandel_pic*)malloc(sizeof(Mandel_pic));
+        } else {
+            free_mandel(mandel);
+        }
+        *mandel = temp_mandel;
+        scale *= 0.95;
+    }
+
+    if (mandel != NULL) {
+        free_mandel(mandel);
+        free(mandel);
+    }
+
+    return 0;
+}
+
